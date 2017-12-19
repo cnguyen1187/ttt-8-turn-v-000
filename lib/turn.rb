93acb74ed9b275da85212378a8f95fd6b98259cd
Cam Nguyen <cnguyen1187@gmail.com>
2017-12-19 19:15:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,15 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-end
+
+  if valid_move?(board, index)
+        move(board, index)
+        display_board(board)
+      else
+        puts "Please try again"
+        turn(board)
+    end
+  end
 
   def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
